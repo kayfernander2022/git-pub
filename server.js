c0a2407eeb0d7e20f0b88cc828901970fb00e8ce
kayfernander2022 <kayfernander@hotmail.com>
2022-11-09 09:48:34 -0500

    database setup complete

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const drinks = require('./models/drinks')//required by your server.js
 PORT = 3000
 
 
@@ -8,7 +8,11 @@ app.get("/", (req, res) => {
   res.send('Welcome to the GitPub App!');
 });
 
+app.get('/drinks', (req,res)=>{
+  res.send(drinks)
+})
 
+//app.use(drinks);//middleware to use the routes the fruits in controller folder
 
 app.listen(3000, () => {
   console.log('We are now listening on port 3000');
